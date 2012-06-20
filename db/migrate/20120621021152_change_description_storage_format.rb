eeaf6291194efed7d2df3fc6d4a179e4e8ff7f14
Azamat Khudaygulov <azamat@hudaygulov.ru>
2012-06-21 02:24:10 +0400

    Add migration to change description checkout storage format

---
+++ db/migrate/20120621021152_change_description_storage_format.rb
@@ -0,0 +1,27 @@
+class ChangeDescriptionStorageFormat < ActiveRecord::Migration
+  class Repository < ActiveRecord::Base
+    serialize :checkout_settings, Hash
+
+    # disable single table inheritance
+    def self.inheritance_column() nil end
+    # to fix some strange error where the type did return the class...
+    def type() attributes["type"] end
+  end
+
+  def self.up
+    ## First migrate the individual repositories
+    Repository.all.each do |r|
+      next unless !r.checkout_settings.empty?
+      r.checkout_settings['checkout_description'].force_encoding("UTF-8")
+      r.save!
+    end
+
+    ## Then the global settings
+    settings = Setting.plugin_redmine_checkout
+    settings["description_Abstract"].force_encoding("UTF-8")
+    Setting.plugin_redmine_checkout = settings
+  end
+
+  def self.down
+  end
+end
