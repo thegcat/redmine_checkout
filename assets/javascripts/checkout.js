367c939ad4efc9fad1ef35e7d0b86b40713bcc67
Raphael Kallensee <raphael@kallensee.name>
2012-10-30 00:38:10 +0100

    More fixes for frontend compatibility to 2.1.x.

--- assets/javascripts/checkout.js
+++ assets/javascripts/checkout.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
   /* update the checkout URL if clicked on a protocol */
   $('#checkout_box #checkout_protocols a').each(function(i) {
     $(this).bind('click', function(event) {
-      $('input#checkout_url').val(checkout_commands.get($(this).attr('id')));
+      $('input#checkout_url').val(checkout_commands[$(this).attr('id')]);
       $('#checkout_box #checkout_protocols a').removeClass("selected");
       $(this).addClass("selected")
 
       var access = $('span#checkout_access');
       if (access.size() > 0) {
-        var value = window.checkout_access.get($(this).attr('id'));
+        var value = window.checkout_access[$(this).attr('id')];
         access.html(value);
       }
 
